9fd4f41bbe95f3abb7eef23b7fcf3af7001eab7d
Sam Estrin <sam.estrin@gmail.com>
2024-05-14 13:07:35 -0700

    Added Comments and isURL validation

--- netlify/edge-functions/shorten.ts
+++ netlify/edge-functions/shorten.ts
@@ -1,9 +1,22 @@
 import { multiParser } from "https://deno.land/x/multiparser@0.114.0/mod.ts";
+import { isURL } from "https://deno.land/x/is_url/mod.ts";
+
 import { headers } from "./headers.ts";
 import handler from "./utils.ts";
 const urlBase = Deno.env.get("URL_BASE") || "";
 const { generateShortUrl } = handler();
 
+/**
+ * Redirects to the long URL associated with the given short URL.
+ *
+ * @param request - The incoming HTTP request.
+ * @returns A redirection response to the long URL or an error message.
+ * @throws If an error occurs while fetching the long URL from Supabase.
+ *
+ * @example
+ * // Example usage
+ * curl -X GET https://your-api-url/shortUrl
+ */
 export default async (request: Request): Promise<Response> => {
   try {
     const formData = await multiParser(request);
@@ -11,6 +24,13 @@ export default async (request: Request): Promise<Response> => {
 
     if (!url) {
       return new Response(JSON.stringify({ error: "No URL provided" }), {
+        status: 400,
+        headers,
+      });
+    }
+
+    if (!isURL(url)) {
+      return new Response(JSON.stringify({ error: "Invalid URL provided" }), {
         status: 400,
         headers,
       });
