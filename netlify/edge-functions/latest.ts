57c3d0151e9f3458dd73fe403b69c28b838d70d4
Sam Estrin <sam.estrin@gmail.com>
2024-05-13 20:53:45 -0700

    Update latest.ts

--- netlify/edge-functions/latest.ts
+++ netlify/edge-functions/latest.ts
@@ -1,6 +1,7 @@
 import { headers } from "./headers.ts";
 import handler from "./utils.ts";
 
+const urlBase = Deno.env.get("URL_BASE") ? Deno.env.get("URL_BASE") : "";
 const { fetchFromSupabase } = handler();
 
 export default async (request: Request): Promise<Response> => {
@@ -11,7 +12,12 @@ export default async (request: Request): Promise<Response> => {
       `urls?order=created_at.desc&limit=${count}`,
       { method: "GET" }
     );
-    return new Response(JSON.stringify(data), { status: 200, headers });
+    const modifiedData = data.map((item: any) => ({
+      ...item,
+      short_url: `${urlBase}/${item.short_url}`,
+    }));
+
+    return new Response(JSON.stringify(modifiedData), { status: 200, headers });
   } catch (err) {
     return new Response(
       JSON.stringify({ error: "Internal server error", details: err.message }),
